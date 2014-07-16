e0cb03c325e85ec04aa5515023a3656040ab66a3
Adam Engström <adaengst@gmail.com>
2014-07-16 16:30:00 +0200

    Add persistant storage of application settings, add preferences menu for changing settings

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@
 # Todos:
 # - Keychain integration
 # - move out activities to main menu?
+# - cyclic check of current activity
 #
 import rumps
 import control
@@ -9,15 +10,20 @@ import config
 import json
 
 class Harmenubar(rumps.App):
+    APP_SETTINGS = 'settings.json'
+    REMOTE_CONFIG = 'remote.json'
+
     def __init__(self):
         super(Harmenubar,self).__init__('Harmenubar',icon='resources/icon.png')
-        self.activity = 5337127
+
+        self.settings = self.load_config_file(self.APP_SETTINGS)
+        if self.settings is None:
+            self.settings = self.settings_popup()
+
         self.auth()
 
-        try:
-            with self.open('config.json','r') as f:
-                cfg = json.loads(f.read())
-        except IOError:
+        cfg = self.load_config_file(self.REMOTE_CONFIG)
+        if cfg is None:
             cfg = self.get_and_store_config()
 
         self.cfg = config.HarmonyConfig(cfg)
@@ -30,24 +36,66 @@ class Harmenubar(rumps.App):
             rumps.MenuItem('Power Off', callback=lambda m: self.set_activity(m,-1)),
             {'Activity': self.build_activity_menu()},
             {'Device': self.build_device_menu()},
+            None,
+            rumps.MenuItem('Preferences', callback=lambda m: self.settings_popup())
         ]
         self.update_current_activity(self.activity)
 
+    def load_config_file(self, fname):
+        try:
+            with self.open(fname,'r') as f:
+                return json.loads(f.read())
+        except IOError:
+            return None
+
+    def settings_popup(self):
+        settings = {'username':'',
+                    'password':'',
+                    'harmony_ip':'',}
+        w = rumps.Window('')
+        w.title = 'Harmenubar settings'
+        if self.settings is None:
+            w.default_text = json.dumps(settings, indent=4)
+        else:
+            w.default_text = json.dumps(self.settings, indent=4)
+        w.add_button('Cancel')
+        r = w.run()
+        if r.clicked == 1:
+            self.settings = json.loads(r.text)
+            self.save_settings()
+
+    def save_settings(self):
+        with self.open(self.APP_SETTINGS, 'w') as f:
+            f.write(json.dumps(self.settings))
+
     def auth(self):
-        print 'Logging in to Logitech..'
-        self.session_token = control.login_to_logitech('adreg@megalan.org', 'ankeborg', '192.168.0.158')
+        if 'auth_token' in self.settings:
+            print 'Re-using session token from config'
+            self.session_token = self.settings['auth_token']
+        else:
+            print 'Logging in to Logitech..'
+            self.session_token = control.login_to_logitech(
+                self.settings['username'], 
+                self.settings['password'], 
+                self.settings['harmony_ip'])
+            self.settings['auth_token'] = self.session_token
+            self.save_settings()
+        print self.session_token
         print 'Connecting client..'
-        client = control.get_client('192.168.0.158', self.session_token)
+        client = self.get_client()
         print 'Get current activity..'
         self.activity = client.get_current_activity()
         client.disconnect(send_close=True)
 
+    def get_client(self):
+        return control.get_client(self.settings['harmony_ip'], self.session_token)
+
     def get_and_store_config(self):
-        client = control.get_client('192.168.0.158', self.session_token)
-        print 'Get configuration..'
+        client = self.get_client()
+        print 'Getting configuration..'
         cfg = client.get_config()
         client.disconnect(send_close=True)
-        with self.open('config.json','w') as f:
+        with self.open(self.REMOTE_CONFIG,'w') as f:
             f.write(json.dumps(cfg))
         return cfg
 
@@ -68,7 +116,7 @@ class Harmenubar(rumps.App):
 
     def set_activity(self, menu,id_):
         print 'Setting activity:', menu,id_
-        client = control.get_client('192.168.0.158', self.session_token)
+        client = self.get_client()
         print 'call start_activity'
         client.start_activity(id_)
         client.disconnect(send_close=True)
